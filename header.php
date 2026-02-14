b7747dfde1884f540cd0761ccf1aac943b6a7c89
Neurot <em.papaefthimiou@gmail.com>
2026-02-14 23:21:20 +0200

    fix: dynamic header with login/logout; make index use header/footer; add logout

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
+<?php
+require_once 'functions.php';
+?>
 <!DOCTYPE html>
 <html lang="el">
 <head>
   <meta charset="UTF-8">
   <title>Vinyl Records Store</title>
   <link rel="stylesheet" href="assets/css/style.css">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 <body>
 
@@ -13,7 +17,14 @@
     <a href="index.php">Αρχική</a>
     <a href="shop.php">Κατάστημα</a>
     <a href="cart.php">Καλάθι</a>
-    <a href="login.php">Σύνδεση</a>
+    <?php if (isLoggedIn()):
+        $u = getCurrentUser();
+    ?>
+      <span style="color:#fff;margin-left:10px;">Χαίρετε, <?php echo htmlspecialchars($u['username']); ?></span>
+      <a href="logout.php">Αποσύνδεση</a>
+    <?php else: ?>
+      <a href="login.php">Σύνδεση</a>
+    <?php endif; ?>
   </nav>
 </header>
 
